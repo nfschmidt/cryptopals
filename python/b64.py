970ce3a8f6babfe231d06a068f0e0f63570b3371
Nicolas Schmidt <nfschmidt@gmail.com>
2017-06-06 22:03:12 -0300

    Finished b64 module

--- python/b64.py
+++ python/b64.py
@@ -4,39 +4,65 @@ from itertools import zip_longest
 
 SYMBOLS = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 PADDING_SYMBOL = b'='[0]
+VALUES = {s: i for (i, s) in enumerate(SYMBOLS)}
 
-def from_bytes(rawbytes):
-    for t in triplets_groups(rawbytes):
-        yield from from_triplet(t)
 
-def triplets_groups(rawbytes):
-    iterables = [iter(rawbytes)] * 3
+def encode(rawbytes):
+    for triplet in tuples_of(3, rawbytes):
+        yield SYMBOLS[triplet[0] >> 2]
+
+        if triplet[1] != None:
+            yield SYMBOLS[((triplet[0] & 0x03) << 4) | triplet[1] >> 4]
+
+            if triplet[2] != None:
+                yield SYMBOLS[((triplet[1] & 0x0F) << 2) | triplet[2] >> 6]
+                yield SYMBOLS[(triplet[2] & 0x3F)]
+            else:
+                yield SYMBOLS[(triplet[1] & 0x0F) << 2]
+                yield PADDING_SYMBOL
+        else:
+            yield SYMBOLS[(triplet[0] & 0x03) << 4]
+            yield PADDING_SYMBOL
+            yield PADDING_SYMBOL
+
+def decode(b64in):
+    for quad in tuples_of(4, b64in):
+        b1, b2, b3, b4 = [VALUES.get(b, None) for b in quad]
+        yield (b1 << 2) | (b2 >> 4)
+
+        if quad[2] != PADDING_SYMBOL:
+            yield ((b2 << 4) & 0xFF) | (b3 >> 2)
+
+        if quad[3] != PADDING_SYMBOL:
+            yield ((b3 << 6) & 0xFF) | b4
+
+def tuples_of(length, iterable):
+    iterables = [iter(iterable)] * length
     yield from zip_longest(*iterables, fillvalue=None)
 
-def from_triplet(triplet):
-    assert len(triplet) == 3
+def _error(*args, **kwargs):
+    print("Error: invalid argument(s)", file=sys.stderr)
+    print("Usage: {} [encode | decode]".format(sys.argv[0]), file=sys.stderr)
+    exit(1)
 
-    yield SYMBOLS[triplet[0] >> 2]
-
-    if triplet[1] != None:
-        yield SYMBOLS[((triplet[0] & 0x03) << 4) | triplet[1] >> 4]
-
-        if triplet[2] != None:
-            yield SYMBOLS[((triplet[1] & 0x0F) << 2) | triplet[2] >> 6]
-            yield SYMBOLS[(triplet[2] & 0x3F)]
-        else:
-            yield SYMBOLS[(triplet[1] & 0x0F) << 2]
-            yield PADDING_SYMBOL
-    else:
-        yield SYMBOLS[(triplet[0] & 0x03) << 4]
-        yield PADDING_SYMBOL
-        yield PADDING_SYMBOL
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        action = encode
+    elif len(sys.argv) != 2:
+        action = _error
+    elif sys.argv[1] == 'encode':
+        action = encode
+    elif sys.argv[1] == 'decode':
+        action = decode
+    else:
+        action = _error
+
+
     def stdin_bytes():
         for i in sys.stdin.buffer:
             yield from i
 
-    for b in from_bytes(stdin_bytes()):
-        print(chr(b), end='')
+    for b in action(stdin_bytes()):
+        sys.stdout.buffer.write(bytes([b]))
--- python/test_b64.py
+++ python/test_b64.py
@@ -6,8 +6,8 @@ import b64
     (b'7Zg', b'N1pn'),
     (b'AzU', b'QXpV'),
 ])
-def test__from_bytes__with_3_bytes(bytes_input, expected):
-    out = b64.from_bytes(bytes_input)
+def test__encode__with_3_bytes(bytes_input, expected):
+    out = b64.encode(bytes_input)
     result = b''.join(bytes([b]) for b in out)
 
     assert  expected == result
@@ -17,8 +17,8 @@ def test__from_bytes__with_3_bytes(bytes_input, expected):
     (b'ADr86 ', b'QURyODYg'),
     (b'p^.k!s', b'cF4uayFz'),
 ])
-def test__from_bytes__with_bytes_multiple_of_3(bytes_input, expected):
-    out = b64.from_bytes(bytes_input)
+def test__encode__with_bytes_multiple_of_3(bytes_input, expected):
+    out = b64.encode(bytes_input)
     result = b''.join(bytes([b]) for b in out)
 
     assert  expected == result
@@ -28,8 +28,8 @@ def test__from_bytes__with_bytes_multiple_of_3(bytes_input, expected):
     (b'ADr8 ', b'QURyOCA='),
     (b'p^.k!', b'cF4uayE='),
 ])
-def test__from_bytes__1_padding(bytes_input, expected):
-    out = b64.from_bytes(bytes_input)
+def test__encode__1_padding(bytes_input, expected):
+    out = b64.encode(bytes_input)
     result = b''.join(bytes([b]) for b in out)
 
     assert  expected == result
@@ -39,8 +39,52 @@ def test__from_bytes__1_padding(bytes_input, expected):
     (b'AD r', b'QUQgcg=='),
     (b'p^.}', b'cF4ufQ=='),
 ])
-def test__from_bytes__2_paddings(bytes_input, expected):
-    out = b64.from_bytes(bytes_input)
+def test__encode__2_paddings(bytes_input, expected):
+    out = b64.encode(bytes_input)
+    result = b''.join(bytes([b]) for b in out)
+
+    assert  expected == result
+
+@pytest.mark.parametrize("b64_input,expected", [
+    (b'MTIz', b'123'),
+    (b'N1pn', b'7Zg'),
+    (b'QXpV', b'AzU'),
+])
+def test__decode__with_input_of_lenght_4(b64_input, expected):
+    out = b64.decode(b64_input)
+    result = b''.join(bytes([b]) for b in out)
+
+    assert  expected == result
+
+@pytest.mark.parametrize("b64_input,expected", [
+    (b'cFNqM0th', b'pSj3Ka'),
+    (b'QURyODYg', b'ADr86 '),
+    (b'cF4uayFz', b'p^.k!s'),
+])
+def test__decode__without_padding(b64_input, expected):
+    out = b64.decode(b64_input)
+    result = b''.join(bytes([b]) for b in out)
+
+    assert  expected == result
+
+@pytest.mark.parametrize("b64_input,expected", [
+    (b'cFNqM0s=', b'pSj3K'),
+    (b'QURyOCA=', b'ADr8 '),
+    (b'cF4uayE=', b'p^.k!'),
+])
+def test__decode__with_1_padding(b64_input, expected):
+    out = b64.decode(b64_input)
+    result = b''.join(bytes([b]) for b in out)
+
+    assert  expected == result
+
+@pytest.mark.parametrize("b64_input,expected", [
+    (b'cFNqSw==', b'pSjK'),
+    (b'QUQgcg==', b'AD r'),
+    (b'cF4ufQ==', b'p^.}'),
+])
+def test__decode__with_2_padding(b64_input, expected):
+    out = b64.decode(b64_input)
     result = b''.join(bytes([b]) for b in out)
 
     assert  expected == result
